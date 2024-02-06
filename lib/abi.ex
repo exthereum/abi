dfe5cfa13f20160029e25e2db1ab55c02be2dade
Geoffrey Hayes <hayesgm@gmail.com>
2024-02-05 22:18:36 -0800

    Parse Tuples from ABI JSON
    This patch allows us to parse tuples from ABI json, e.g. from structs. We parse them as structs, if we can, which retain information about their variable names, but otherwise fallback to parsing them as :tuples, which are purely positional. The encoder and decoder are indifferent to the difference between structs and tuples and encode/decode them identically.

--- lib/abi.ex
+++ lib/abi.ex
@@ -103,8 +103,8 @@ defmodule ABI do
       iex> File.read!("priv/dog.abi.json")
       ...> |> Jason.decode!
       ...> |> ABI.parse_specification
-      [%ABI.FunctionSelector{function: "bark", returns: nil, types: [:address, :bool]},
-       %ABI.FunctionSelector{function: "rollover", returns: :bool, types: []}]
+      [%ABI.FunctionSelector{function: "bark", returns: nil, types: [:address, :bool], names: ["at", "loudly"]},
+       %ABI.FunctionSelector{function: "rollover", returns: :bool, types: [], names: []}]
 
       iex> [%{
       ...>   "constant" => true,
@@ -119,7 +119,7 @@ defmodule ABI do
       ...>   "type" => "function"
       ...> }]
       ...> |> ABI.parse_specification
-      [%ABI.FunctionSelector{function: "bark", returns: nil, types: [:address, :bool]}]
+      [%ABI.FunctionSelector{function: "bark", returns: nil, types: [:address, :bool], names: ["at", "loudly"]}]
 
       iex> [%{
       ...>   "inputs" => [
@@ -141,7 +141,7 @@ defmodule ABI do
       ...>   "type" => "fallback"
       ...> }]
       ...> |> ABI.parse_specification
-      [%ABI.FunctionSelector{function: nil, returns: nil, types: []}]
+      [%ABI.FunctionSelector{function: nil, returns: nil, types: [], names: []}]
   """
   def parse_specification(doc) do
     doc
