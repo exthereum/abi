4def8bbd9f05b5977135887032d5769c36a915f5
fluffywaffles <skorlir@gmail.com>
2025-01-16 16:11:51 -0800

    chore: increment minor version to include integer encoder

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ABI.Mixfile do
   def project do
     [
       app: :abi,
-      version: "1.1.0",
+      version: "1.2.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       description: "Ethereum's ABI Interface",
